4d008a622b900e955b5d9768e262a628433159ed
Santiago Nule <sn1964a@student.american.edu>
2021-02-22 09:06:57 -0500

    precheck

--- R/fbind.R
+++ R/fbind.R
@@ -1,5 +1,13 @@
+#' Combine levels of two factors
+#'
+#' @param a  a factor
+#' @param b  a factor
+#'
+#' @return  return a factor with the levels of a and b
+#' @export  fbind
+#'
+#' @examples
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
-# Test it
-fbind(as.factor(c("dog","cat")),as.factor(c("gerbil","parakeet")))
+
